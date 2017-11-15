54b04ae54d83a479722bb51cf7e4b3dcbf42251f
johanringstrom <johan.ringstrom@gmail.com>
2017-11-15 14:05:17 +0100

    Gitignore file was added

--- src/johrin7/SavingsAccount.java
+++ src/johrin7/SavingsAccount.java
@@ -2,5 +2,12 @@ package johrin7;
 
 public class SavingsAccount {
 	
-	
+	private double interest;
+	private double balance;
+	private int lastAccountNr = 0;
+		
+		SavingsAccount() {
+			this.interest = 0.11;
+			this.balance = 0;
+		}
 }
